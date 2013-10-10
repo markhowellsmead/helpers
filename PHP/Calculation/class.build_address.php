50389bfe2f5fd8cb3503c48a4bf873e16121481f
Mark Howells-Mead <permanent.tourist@gmail.com>
2013-10-10 16:50:19 +0200

    Revise code to remove TYPO3-specific function calls

--- PHP/Calculation/class.build_address.php
+++ PHP/Calculation/class.build_address.php
@@ -4,12 +4,12 @@ class AddressHelpers {
 
 	var $viewData = array();
 
-	function build_address($data=array(),$field_keys=array(),$glue=''){
+	function build_address($data=array(),$keys=array(),$glue=''){
 		/*
 			Build a HTML string for displaying an address on the website
 			Conditionals built-in to determine line-breaks etc.
 			
-			WARNING: requires function force_attributes to be defined!
+			WARNING: requires functions force_attributes and encode_email to be defined!
 			
 			Usage: 
 			
@@ -19,7 +19,7 @@ class AddressHelpers {
 				'town' => $veranstaltungArray['ort']
 			);
 			
-			$field_keys = array(
+			$keys = array(
 				'street' 	=> 'strasse',
 				'postcode' 	=> 'plz',
 				'town' 		=> 'ort'
@@ -27,14 +27,14 @@ class AddressHelpers {
 			
 			$AddressHelpers = new AddressHelpers();
 			
-			echo '<p>'.$AddressHelpers->build_address($address_data, $field_keys, '<br>').'</p>';
+			echo '<p>'.$AddressHelpers->build_address($address_data, $keys, '<br>').'</p>';
 			
 			Once the function has been called, the compiled address (with e.g. empty 
 			values omitted) is available from then on as $myaddress->viewData['address']
 			
 		*/
 
-		$field_keys = $this->force_attributes(array(
+		$keys = $this->force_attributes(array(
 			'company' 	=> 'veranstalter',
 			'person' 	=> 'person',
 			'street' 	=> 'street',
@@ -45,31 +45,31 @@ class AddressHelpers {
 			'telephone' => 'telephone',
 			'fax' 		=> 'fax',
 			'mobile' 	=> 'mobile'
-		), $field_keys);
+		), $keys);
 	
 		$this->viewData['address'] = array();
 		
-		if(!empty($data[$field_keys['company']])){$this->viewData['address'][] = '<strong>' .$data[$field_keys['company']]. '</strong>';}
-		if(!empty($data[$field_keys['person']])){$this->viewData['address'][] = $data[$field_keys['person']];}
+		if(!empty($data[$keys['company']])){$this->viewData['address'][] = '<strong>' .$data[$keys['company']]. '</strong>';}
+		if(!empty($data[$keys['person']])){$this->viewData['address'][] = $data[$keys['person']];}
 	
-		if(!empty($data[$field_keys['street']])){$this->viewData['address'][] = $data[$field_keys['street']];}
+		if(!empty($data[$keys['street']])){$this->viewData['address'][] = $data[$keys['street']];}
 	
-		if(!empty($data[$field_keys['postcode']]) && !empty($data[$field_keys['town']])){
-			$this->viewData['address'][] = $data[$field_keys['postcode']] . (!empty($data[$field_keys['postcode']]) && !empty($data[$field_keys['town']]) ? ' ' : '') . $data[$field_keys['town']];
+		if(!empty($data[$keys['postcode']]) && !empty($data[$keys['town']])){
+			$this->viewData['address'][] = $data[$keys['postcode']] . (!empty($data[$keys['postcode']]) && !empty($data[$keys['town']]) ? ' ' : '') . $data[$keys['town']];
 		}
 	
-		if(!empty($data[$field_keys['telephone']])){$this->viewData['address'][] = $this->pi_getLL('tx_frpveranstaltungen_veranstaltung.telefon').': '.$data[$field_keys['telephone']];}
-		if(!empty($data[$field_keys['fax']])){$this->viewData['address'][] = $this->pi_getLL('tx_frpveranstaltungen_veranstaltung.fax').': '.$data[$field_keys['fax']];}
-		if(!empty($data[$field_keys['mobile']])){$this->viewData['address'][] = $this->pi_getLL('tx_frpveranstaltungen_veranstaltung.mobile').': '.$data[$field_keys['mobile']];}
+		if(!empty($data[$keys['telephone']])){$this->viewData['address'][] = $this->pi_getLL('tx_frpveranstaltungen_veranstaltung.telefon').': '.$data[$keys['telephone']];}
+		if(!empty($data[$keys['fax']])){$this->viewData['address'][] = $this->pi_getLL('tx_frpveranstaltungen_veranstaltung.fax').': '.$data[$keys['fax']];}
+		if(!empty($data[$keys['mobile']])){$this->viewData['address'][] = $this->pi_getLL('tx_frpveranstaltungen_veranstaltung.mobile').': '.$data[$keys['mobile']];}
 	
-		if($this->valid_email_address($data[$field_keys['email']])){
-			$mailArray = $this->cObj->getMailTo($data[$field_keys['email']],$data[$field_keys['email']]);
-			$this->viewData['address'][] = '<a href="'.$mailArray[0].'">'.$mailArray[1].'</a>';
+		if($this->valid_email_address($data[$keys['email']])){
+			$this->encode_email($data[$keys['email']]);
+			$this->viewData['address'][] = '<a href="mailto:'.$data[$keys['email']].'">'.$data[$keys['email']].'</a>';
 		}
 		
-		if($this->valid_url($data[$field_keys['url']])){
-			$displayURL = parse_url($data[$field_keys['url']]);
-			$this->viewData['address'][] = '<a href="' .$data[$field_keys['url']]. '">' .$displayURL['host']. '</a>';
+		if($this->valid_url($data[$keys['url']])){
+			$displayURL = parse_url($data[$keys['url']]);
+			$this->viewData['address'][] = '<a href="' .$data[$keys['url']]. '">' .$displayURL['host']. '</a>';
 		}
 		
 		return implode($glue,$this->viewData['address']);
