751f07656627bd70b205d5042ac421d5d519600a
Mark Howells-Mead <permanent.tourist@gmail.com>
2014-05-15 15:47:34 +0200

    Update JS to use class-based functionality

--- JavaScript/form-to-localstorage/store.js
+++ JavaScript/form-to-localstorage/store.js
@@ -1,9 +1,24 @@
+var frp_offlineapp = function(){
+
+	'use strict';
+	
+	this.storeFormValues = function(wrapper){
+		var data = Array();
+		$('div.person',$(wrapper)).each(function(){
+			var $form = $('form',$(this));
+			data.push($form.serializeArray());
+		})
+		localStorage.setItem('form-to-localstorage', JSON.stringify(data));
+		$('#code').html(localStorage.getItem('form-to-localstorage'));
+		
+	};//storeFormValues
+	
+};//frp_offlineapp
+	
+//////////////////////////////////////////////////
+	
+var app = new frp_offlineapp();
+
 $('div.people').bind('keyup',function(e){
-	data = Array();
-	$('div.person',$(this)).each(function(){
-		$form = $('form',$(this));
-		data.push($form.serializeArray());
-	})
-	localStorage.setItem('form-to-localstorage', JSON.stringify(data));
-	$('#code').html(localStorage.getItem('form-to-localstorage'));
+	app.storeFormValues(this);
 });
