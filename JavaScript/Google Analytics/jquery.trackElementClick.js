e1b526a535ae0aadbfd498b31fbeba72ed5fdf56
Mark Howells-Mead <m@mhm.li>
2016-03-24 23:55:58 +0100

    Function naming and file whitespace

--- JavaScript/Google Analytics/jquery.trackElementClick.js
+++ JavaScript/Google Analytics/jquery.trackElementClick.js
@@ -1,6 +1,6 @@
 (function($, undefined){
 
-    /**
+	/**
 	* Add click event handler to an element / multiple elements
 	* which pings Google Analytics when the element is clicked.
 	* Requires jQuery.
@@ -10,29 +10,25 @@
 	* @since 	24/03/16
 	*/
 
-    $.fn.extend({
-		trackTabClicks: function() {
-    		$(this).each(function(){
-                $(this).on('click.GA', function(){
-                    try {
-                        ga && ga('send', {
-                            hitType: 'event',
-                            eventCategory: 'Posts recent',
-                            eventAction: 'clicked',
-                            eventLabel: $(this).text()
-                        });
-                    }catch(e){}
-                });
-            });
+	$.fn.extend({
+		ga_trackElementClick: function() {
+			$(this).each(function(){
+				$(this).on('click.GA', function(){
+					try {
+						ga && ga('send', {
+							hitType: 'event',
+							eventCategory: 'Posts recent',
+							eventAction: 'clicked',
+							eventLabel: $(this).text()
+						});
+					}catch(e){}
+				});
+			});
 			return this;
 		}
 	});
-	
 
-
-    // example usage
-	jQuery(document).ready(function($) {
-		$('.posts-recent a').trackTabClicks();
-	});
+	// example usage
+	$('.posts-recent a').ga_trackElementClick();
 
 })(jQuery);
