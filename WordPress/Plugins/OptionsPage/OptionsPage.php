d32b3f6fa3a9b1f8ab33661e5f48b8a5e7239bba
Mark Howells-Mead <permanent.tourist@gmail.com>
2016-08-26 09:55:59 +0200

    WordPress plugin options page

---
+++ WordPress/Plugins/OptionsPage/OptionsPage.php
@@ -0,0 +1,78 @@
+<?php
+/*
+    Add a plugin options page to the WordPress backend. 
+    Uses PHP namespaces, so compatible with PHP 5.3+ only!
+
+    Use this file in your plugin and include it using the following code,
+    placed in the __construct function of the main class. Make sure that 
+    the PHP namespace in this file is the same as the PHP namespace in the
+    main class file!
+
+    if (is_admin()) {
+        require_once 'OptionsPage.php';
+        new OptionsPage();
+    }
+
+    Author: Mark Howells-Mead, permanenttourist.ch, August 2016
+    License: https://creativecommons.org/licenses/by/4.0/
+
+ */
+
+namespace MHM\MyGreatPlugin;
+
+class OptionsPage
+{
+    public $key = '';
+
+    public function __construct()
+    {
+        $this->key = basename(__DIR__);
+        add_action('admin_menu', array($this, 'createAdminMenu'));
+    }
+
+    public function createAdminMenu()
+    {
+        // Top-level menu. Uses the dashicons font to generate the icon “dashicons-images-alt”
+        // add_menu_page('Custom Menu Page Title', 'Custom Menu Page', 'manage_options', __FILE__, array($this, 'settingsPage'), 'dashicons-images-alt', 90);
+
+        // Secondary-level menu; a sub-page of Settings in this example.
+        add_submenu_page('options-general.php', _x('Custom menu page', 'Label for page title', $this->key), _x('Custom menu page', 'Label for menu entry', $this->key), 'manage_options', 'custommenupage', array($this, 'settingsPage'));
+
+        //call register settings function
+        add_action('admin_init', array($this, 'registerPluginSettings'));
+    }
+
+    public function registerPluginSettings()
+    {
+        //register our settings
+        register_setting($this->key.'-settings-contact', 'contact_email');
+    }
+
+    public function settingsPage()
+    {
+        echo '<div class="wrap">
+            <h1>'._x('Custom menu page', 'Label for page title', $this->key).'</h1>
+            <form method="post" action="options.php">
+            ';
+
+        settings_fields($this->key.'-settings-contact');
+        do_settings_sections($this->key.'-settings-contact');
+
+        echo '<table class="form-table">
+        <tr>
+        <th scope="row">
+            <tr valign="top">
+                <th scope="row">'.__('Email address for contact requests', $this->key).'</th>
+                <td>
+                    <input id="contact_email" type="email" name="contact_email" value="'.esc_attr(get_option('contact_email')).'" class="regular-text ltr" />
+                    <p class="description" id="contact_email-description">'.__('This email address will be used for functions which allow the website visitor to contact the owner of the website.', $this->key).'</p>
+                </td>
+            </tr>
+        </table>';
+
+        submit_button();
+
+        echo '</form>
+        </div>';
+    }
+}
