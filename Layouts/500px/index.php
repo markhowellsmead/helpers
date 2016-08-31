4a68d36bc0826ac4faa4c5c5fead85d854442165
Mark Howells-Mead <permanent.tourist@gmail.com>
2016-08-31 17:58:37 +0200

    500px grid layout :D

---
+++ Layouts/500px/index.php
@@ -0,0 +1,316 @@
+<?php
+
+
+$photos = json_decode('[
+  {
+    "url": "photo-103450229.jpg",
+    "width": 675,
+    "height": 900
+  },
+  {
+    "url": "photo-108273877.jpg",
+    "width": 1170,
+    "height": 780
+  },
+  {
+    "url": "photo-115203323.jpg",
+    "width": 1170,
+    "height": 780
+  },
+  {
+    "url": "photo-23583825.jpg",
+    "width": 2048,
+    "height": 1536
+  },
+  {
+    "url": "stock-photo-123942383.jpg",
+    "width": 2000,
+    "height": 1333
+  },
+  {
+    "url": "stock-photo-124559545.jpg",
+    "width": 2000,
+    "height": 1333
+  },
+  {
+    "url": "stock-photo-132046989.jpg",
+    "width": 1170,
+    "height": 780
+  },
+  {
+    "url": "stock-photo-132118343.jpg",
+    "width": 2000,
+    "height": 1339
+  },
+  {
+    "url": "stock-photo-132311221.jpg",
+    "width": 1920,
+    "height": 1080
+  },
+  {
+    "url": "stock-photo-132586903.jpg",
+    "width": 2000,
+    "height": 1334
+  },
+  {
+    "url": "stock-photo-135203031.jpg",
+    "width": 1000,
+    "height": 668
+  },
+  {
+    "url": "stock-photo-135626379.jpg",
+    "width": 2000,
+    "height": 1500
+  },
+  {
+    "url": "stock-photo-136947953.jpg",
+    "width": 2000,
+    "height": 1348
+  },
+  {
+    "url": "stock-photo-138378295.jpg",
+    "width": 2000,
+    "height": 1333
+  },
+  {
+    "url": "stock-photo-138436811.jpg",
+    "width": 2000,
+    "height": 1333
+  },
+  {
+    "url": "stock-photo-142950305.jpg",
+    "width": 2000,
+    "height": 1125
+  },
+  {
+    "url": "stock-photo-143046061.jpg",
+    "width": 843,
+    "height": 1000
+  },
+  {
+    "url": "stock-photo-143181649.jpg",
+    "width": 2000,
+    "height": 1298
+  },
+  {
+    "url": "stock-photo-144530143.jpg",
+    "width": 2000,
+    "height": 1333
+  },
+  {
+    "url": "stock-photo-144730939.jpg",
+    "width": 1000,
+    "height": 1000
+  },
+  {
+    "url": "stock-photo-145414771.jpg",
+    "width": 780,
+    "height": 1170
+  },
+  {
+    "url": "stock-photo-146038669.jpg",
+    "width": 2000,
+    "height": 1335
+  },
+  {
+    "url": "stock-photo-146231033.jpg",
+    "width": 2000,
+    "height": 1335
+  },
+  {
+    "url": "stock-photo-146914861.jpg",
+    "width": 843,
+    "height": 1000
+  },
+  {
+    "url": "stock-photo-147877407.jpg",
+    "width": 2000,
+    "height": 1334
+  },
+  {
+    "url": "stock-photo-147969173.jpg",
+    "width": 2000,
+    "height": 1333
+  },
+  {
+    "url": "stock-photo-148015373.jpg",
+    "width": 1170,
+    "height": 781
+  },
+  {
+    "url": "stock-photo-148704233.jpg",
+    "width": 1170,
+    "height": 884
+  },
+  {
+    "url": "stock-photo-148928293.jpg",
+    "width": 1170,
+    "height": 781
+  },
+  {
+    "url": "stock-photo-148950715.jpg",
+    "width": 1170,
+    "height": 775
+  },
+  {
+    "url": "stock-photo-21951271.jpg",
+    "width": 2000,
+    "height": 1333
+  },
+  {
+    "url": "stock-photo-21964829.jpg",
+    "width": 2000,
+    "height": 1333
+  },
+  {
+    "url": "stock-photo-22618399.jpg",
+    "width": 2000,
+    "height": 1333
+  },
+  {
+    "url": "stock-photo-31201539.jpg",
+    "width": 2000,
+    "height": 1333
+  },
+  {
+    "url": "stock-photo-34598868.jpg",
+    "width": 542,
+    "height": 768
+  },
+  {
+    "url": "stock-photo-47252094.jpg",
+    "width": 2000,
+    "height": 1333
+  },
+  {
+    "url": "stock-photo-51980510.jpg",
+    "width": 666,
+    "height": 1000
+  },
+  {
+    "url": "stock-photo-55601508.jpg",
+    "width": 666,
+    "height": 1000
+  },
+  {
+    "url": "stock-photo-65681789.jpg",
+    "width": 1840,
+    "height": 1232
+  },
+  {
+    "url": "stock-photo-70461471.jpg",
+    "width": 1000,
+    "height": 1000
+  },
+  {
+    "url": "stock-photo-71801901.jpg",
+    "width": 2000,
+    "height": 1335
+  },
+  {
+    "url": "stock-photo-71913567.jpg",
+    "width": 2000,
+    "height": 1328
+  },
+  {
+    "url": "stock-photo-72223295.jpg",
+    "width": 2000,
+    "height": 1335
+  },
+  {
+    "url": "stock-photo-72620185.jpg",
+    "width": 2000,
+    "height": 1333
+  },
+  {
+    "url": "stock-photo-74402039.jpg",
+    "width": 666,
+    "height": 1000
+  },
+  {
+    "url": "stock-photo-75097491.jpg",
+    "width": 2000,
+    "height": 1333
+  },
+  {
+    "url": "stock-photo-75186237.jpg",
+    "width": 2000,
+    "height": 1333
+  },
+  {
+    "url": "stock-photo-79250373.jpg",
+    "width": 2000,
+    "height": 1325
+  },
+  {
+    "url": "stock-photo-79692589.jpg",
+    "width": 670,
+    "height": 1000
+  },
+  {
+    "url": "stock-photo-7979718.jpg",
+    "width": 1024,
+    "height": 680
+  },
+  {
+    "url": "stock-photo-7980252.jpg",
+    "width": 1024,
+    "height": 680
+  },
+  {
+    "url": "stock-photo-81390687.jpg",
+    "width": 2000,
+    "height": 1591
+  },
+  {
+    "url": "stock-photo-81988949.jpg",
+    "width": 667,
+    "height": 1000
+  },
+  {
+    "url": "stock-photo-83149705.jpg",
+    "width": 775,
+    "height": 1170
+  }
+]', true);
+
+?><!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Cats</title>
+  <link rel="stylesheet" href="500px-grid.css" />
+  <style>
+    .header {
+      padding: 1rem;
+    }
+  </style>
+</head>
+<body>
+<div class="header">
+  <h1>500px grid layout using CSS Flexbox</h1>
+  <p>The code in this example is a simple mutation of the original JavaScript-based Angular solution coded by <a href="https://github.com/xieranmaya/">xieranmaya</a> on Github. He's written an <em>extensive</em> blog post about it <a href="https://github.com/xieranmaya/blog/issues/6">here</a>.</p>
+  <p>Why is this version without JavaScript and Angular? Because if the page is delivered from a server running PHP, there's no need to over-complicate it by introducing JavaScript.</p>
+  <p>Check out the source code of this PHP file to see how easy it is. The true magic lies in the CSS Flexbox code.</p>
+</div>
+  <div class="fav-list">
+<?php
+foreach ($photos as $image) {
+    printf(
+        '<div style="flex-grow:%1$s;flex-basis:%2$spx;">
+          <i style="padding-bottom:%3$s%%"></i>
+          <img src="%4$s%5$s"/>
+        </div>',
+        $image['width'] * 100 / $image['height'],
+        $image['width'] * 360 / $image['height'],
+        $image['height'] / $image['width'] * 100,
+        'https://xieranmaya.github.io/images/cats/',
+        $image['url']
+    );
+}
+
+echo str_repeat('<div class="placeholder"></div>', 10);
+?>
+  </div>
+</body>
+</html>
