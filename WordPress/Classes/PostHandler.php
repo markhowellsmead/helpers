51c38534feebd8cc0376213e6c9e0a1041f48f70
Mark Howells-Mead <permanent.tourist@gmail.com>
2016-08-31 12:16:40 +0200

    Data sanitization and update function

--- WordPress/Classes/PostHandler.php
+++ WordPress/Classes/PostHandler.php
@@ -26,10 +26,26 @@ class PostHandler
         $this->key = basename(__DIR__);
     }
 
+    public function arrayMapR($func, $arr)
+    {
+        $newArr = array();
+
+        foreach ($arr as $key => $value) {
+            $newArr[ $key ] = (is_array($value) ? $this->arrayMapR($func, $value) : (is_array($func) ? call_user_func_array($func, $value) : $func($value)));
+        }
+
+        return $newArr;
+    }
+
+    public function sanitizeData(&$data)
+    {
+        $data = $this->arrayMapR('strip_tags', $data);
+    }
+
     public function createPost($data = array())
     {
         $data = array_merge(array(
-            'post_type' => $this->custom_post_type,
+            'post_type' => 'post',
             'post_author' => get_current_user_id(),
             'post_name' => '',
             'post_title' => '',
@@ -37,6 +53,8 @@ class PostHandler
             'post_status' => 'draft',
             'post_meta' => array(),
         ), $data);
+
+        $this->sanitizeData($data);
 
         if (!$data['post_author']) {
             error_log(__METHOD__.': no valid author');
@@ -50,6 +68,11 @@ class PostHandler
             unset($data['ID']);
         }
 
+        // Remove post meta into its own variable, so that wp_insert_post
+        // doesn't try to save it directly to the post entry. We'll save it separately in a minute.
+        $post_meta = $data['post_meta'];
+        unset($data['post_meta']);
+
         // Store the new post in the database. If all is well, we'll get the new $post_id back.
         $post_id = wp_insert_post($data);
 
@@ -59,17 +82,64 @@ class PostHandler
             return;
         }
 
-        // Adding post meta happens immediately. Values are stored in a separate DB table so we don't need ro re-save the post.
-        if (!empty($data['post_meta'])) {
-            foreach ($data['post_meta'] as $meta_key => $meta_value) {
+        $this->updatePostMeta($post_meta, $post_id);
+
+        return $post_id;
+    }
+
+    public function updatePost($data = array())
+    {
+        $post_meta = null;
+
+        if (!isset($data['ID'])) {
+            error_log(__METHOD__.': ID not specified');
+
+            return;
+        }
+
+        $this->sanitizeData($data);
+
+        // Sanitize data array
+        $data['ID'] = intval($data['ID']);
+
+        // Remove post meta into its own variable, so that wp_insert_post
+        // doesn't try to save it directly to the post entry. We'll save it separately in a minute.
+
+        if (isset($data['post_meta'])) {
+            $post_meta = $data['post_meta'];
+            unset($data['post_meta']);
+        }
+
+        $post_id = wp_update_post($data);
+
+        if (is_wp_error($post_id)) {
+            error_log(__METHOD__.': unable to insert a new post');
+
+            return;
+        }
+
+        $this->updatePostMeta($post_meta, $post_id);
+
+        return $post_id;
+    }
+
+    public function updatePostMeta($post_meta, $post_id)
+    {
+        // Adding post meta happens immediately. Values are stored in a separate
+        // DB table so we don't need ro re-save the post.
+        if (!empty($post_meta)) {
+            foreach ($post_meta as $meta_key => $meta_value) {
                 if (!empty($meta_key)) {
                     if ($meta_value !== false && $meta_value !== null) {
-                        add_post_meta($post_id, $meta_key, $meta_value, true);
+                        $current_value = get_post_meta($post_id, $meta_key, true);
+                        if (empty($current_value)) {
+                            add_post_meta($post_id, $meta_key, $meta_value, true);
+                        } else {
+                            update_post_meta($post_id, $meta_key, $meta_value, $current_value);
+                        }
                     }
                 }
             }
         }
-
-        return $post_id;
     }
 }
