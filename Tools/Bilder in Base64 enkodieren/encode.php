24bc0915369a4d5d2ce825fbbe413094ca0b177b
Mark Howells-Mead <permanent.tourist@gmail.com>
2016-11-06 12:22:45 +0100

    Lower case folder name

--- Tools/Bilder in Base64 enkodieren/encode.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: text/plain; charset=utf-8');
-
-$path= 'Logo_Mail-Sig.png';
-$type = pathinfo($path, PATHINFO_EXTENSION);
-$data = file_get_contents($path);
-$base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
-
-die('<img src="' .$base64. '" alt="" />');
--- Tools/Coda/Clips/README.md
+++
@@ -1,2 +0,0 @@
-#Coda Clips
-Import code clips for various applications for use from sidebar in Coda 2.
