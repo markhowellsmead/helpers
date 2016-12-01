d250c3431cd427baa2e26e35c0a18f2e517200c8
Mark Howells-Mead <permanent.tourist@gmail.com>
2016-12-01 10:59:16 +0100

    JavaScript function getUrlParameter

---
+++ javascript/getUrlParameter.js
@@ -0,0 +1,18 @@
+/**
+ * Get the value of a URL GET parameter
+ * e.g. ?myFoo=bar
+ */
+
+function getUrlParameter(sParam){
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam)
+        {
+            return sParameterName[1];
+        }
+    }
+    return null;
+}
