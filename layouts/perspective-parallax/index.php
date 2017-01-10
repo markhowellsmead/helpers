0353de34e276b4421bcf2c7bd0bfd1e0e6b88750
Mark Howells-Mead <permanent.tourist@gmail.com>
2017-01-10 13:20:10 +0100

    Example of perspective-based CSS parallax

---
+++ layouts/perspective-parallax/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>JS Bin</title>
+  <link rel="stylesheet" href="perspective-parallax.css">
+</head>
+<body>
+  <div class="perspective overflow">
+    <div class="preserve container">
+      <div class="parallax parallax-12">z = -1.2</div>
+      <div class="parallax parallax-10">z = -1.0</div>
+      <div class="parallax parallax-8">z = -0.8</div>
+      <div class="parallax parallax-6">z = -0.6</div>
+      <div class="parallax parallax-4">z = -0.4</div>
+      <div class="parallax parallax-2">z = -0.2</div>
+      <div class="parallax parallax-0">z = 0</div>
+      <div class="spacer"></div>
+    </div>
+  </div>
+</body>
+</html>
