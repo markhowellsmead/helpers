32ca3fce6cb17d537099c63b8838cb3a8d9bd808
Mark Howells-Mead <permanent.tourist@gmail.com>
2018-03-20 17:25:15 +0100

    Update template-csv-export.php

--- web/wordpress/Timber and Twig/Export/template-csv-export.php
+++ web/wordpress/Timber and Twig/Export/template-csv-export.php
@@ -3,12 +3,12 @@
  * Template Name: CSV Export Template
  */
 
+use Timber\Timber;
+
 header('Content-type: text/csv');
 header('Content-Disposition: attachment; filename=data.' .date('Y-m-d-H-i-s'). '.csv');
 header('Pragma: no-cache');
 header('Expires: 0');
-
-+use Timber\Timber;
 
 $context['posts'] = Timber::get_posts(['post_type' => 'my_post_type', 'numberposts' => -1]);
 Timber::render('csv-export.twig', $context);
