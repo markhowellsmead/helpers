e2d8d7950c69d00acadde3768b6adf6c05084104
Mark Howells-Mead <permanent.tourist@gmail.com>
2018-10-18 11:23:18 +0200

    Update FileSystemController.php

--- php/Controllers/FileSystemController.php
+++ php/Controllers/FileSystemController.php
@@ -10,17 +10,16 @@ namespace Mhm\Theme\Controller;
 
 class FileSystemController {
 
-  private static function downloadFile($filepath, $filename)
-    {
-      header('Content-Type: '.mime_content_type($filepath));
-      header('Pragma: public');
-      header('Expires: -1');
-      header('Cache-Control: public, must-revalidate, post-check=0, pre-check=0');
-      header('Content-Disposition: attachment; filename="'.$filename.'"');
-      readfile($filepath);
-      exit;
-    }
-  }
+	private static function downloadFile($filepath, $filename)
+	{
+		header('Content-Type: '.mime_content_type($filepath));
+		header('Pragma: public');
+		header('Expires: -1');
+		header('Cache-Control: public, must-revalidate, post-check=0, pre-check=0');
+		header('Content-Disposition: attachment; filename="'.$filename.'"');
+		readfile($filepath);
+		exit;
+	}
 
 	private static function ensureFolderExists()
 	{
