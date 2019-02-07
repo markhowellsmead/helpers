73798f99dc303c89154d987e44ec341a333eb49c
Mark Howells-Mead <mark@sayhello.ch>
2019-02-07 10:32:55 +0100

    Update swiper.js

--- wordpress/Timber/views/partials/components/swiperjs/swiper.js
+++ wordpress/Timber/views/partials/components/swiperjs/swiper.js
@@ -5,17 +5,6 @@
 
 import Swiper from 'swiper/dist/js/swiper.js';
 
-var appslider = new Swiper('.c-appslider .swiper-container', {
-	autoplay: {
-		delay: 5000
-	},
-	grabCursor: true,
-	speed: 500
-});
-
-var mySwiper = new Swiper('.c-modelsslider .swiper-container', {
-	pagination: {
-		el: '.swiper-pagination',
-		grabCursor: true
-	},
+var mySwiper = new Swiper('.c-swiper .swiper-container', {
+	…
 });
