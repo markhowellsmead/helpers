4d2072cc31d6be5681db5e256f60be254232db5b
Mark Howells-Mead <mark@sayhello.ch>
2019-06-19 09:32:45 +0200

    Create block.jsx

---
+++ wordpress/Gutenberg/Blocks/ServerSide/block.jsx
@@ -0,0 +1,20 @@
+const {ServerSideRender} = wp.components;
+const {__} = wp.i18n;
+
+wp.blocks.registerBlockType('sht/contact-banner', {
+	title: 'Contact Banner',
+	icon: 'email',
+	category: 'sht/blocks',
+
+	edit(props) {
+		return (
+			<ServerSideRender
+				block="sht/contact-banner" // https://heise-medienportal.hello/wp-json/wp/v2/block-renderer/sht/contact-banner?context=edit&lang=de&_locale=user
+				attributes={props.attributes}
+			/>
+		);
+	},
+	save() {
+		return null;
+	},
+});
