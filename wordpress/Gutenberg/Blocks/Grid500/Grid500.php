fca65c0a712217ef44d75b2774d0055bebb7d77a
Mark Howells-Mead <mark@sayhello.ch>
2019-07-15 11:35:21 +0200

    Create Grid500.php

---
+++ wordpress/Gutenberg/Blocks/Grid500/Grid500.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace SayHello\Theme\Block;
+
+use Timber\Timber;
+use Timber\Post as TimberPost;
+
+/**
+ * @author Mark Howells-Mead <mark@sayhello.ch>
+ */
+
+class Grid500
+{
+	public function run()
+	{
+		add_action('init', [$this, 'register']);
+	}
+
+	public function register()
+	{
+		// Block using ACF fields
+		acf_register_block_type([
+			'name' => 'sht/grid500',
+			'category' => 'sht/custom-blocks',
+			'icon' => 'images-alt2',
+			'keywords' => [_x('Galerie', 'Gutenberg block keyword', 'sha'), _x('Bildergalerie', 'Gutenberg block keyword', 'sha'), _x('Bild', 'Gutenberg block keyword', 'sha')],
+			'post_types' => ['post', 'page'],
+			'supports' => [
+				'align' => ['wide', 'full'],
+				'mode' => false
+			],
+			'title' => _x('Bildergalerie (Flex-Layout)', 'Block title', 'sha'),
+			'description' => __('Eine Bildgalerie, in der alle Bilder den verfügbaren Platz füllen.', 'Block description', 'sha'),
+			'render_callback' => function ($block, $content = '', $is_preview = false) {
+				$context = Timber::get_context();
+				$context['block'] = $block;
+				$context['block']['data']['entries'] = get_field('entries', $context['block']['id']);
+				$context['block']['is_edit_mode'] = $is_preview;
+				Timber::render('blocks/grid500.twig', $context);
+			},
+		]);
+	}
+}
