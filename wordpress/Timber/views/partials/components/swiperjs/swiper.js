5d3a40216b7f4e320cf69ef19aabb94d5ae8485c
Mark Howells-Mead <mark@sayhello.ch>
2019-11-24 14:47:19 +0100

    Update swiper.js

--- wordpress/Timber/views/partials/components/swiperjs/swiper.js
+++ wordpress/Timber/views/partials/components/swiperjs/swiper.js
@@ -3,9 +3,11 @@
  * Use npm i swiper -save-dev to install the library dependency
  * Note the import path, which works around problems with gulp-uglify
  * https://github.com/markhowellsmead/helpers/wiki/Swiper
+	*
+	* JS path in the Node Module changed in version 5.
  */
 
-import Swiper from 'swiper/dist/js/swiper.js';
+import Swiper from 'swiper/js/swiper.js';
 
 new Swiper('.swiper', {
 	…
