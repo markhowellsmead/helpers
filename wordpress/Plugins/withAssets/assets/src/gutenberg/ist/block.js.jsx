eaf978ec3bd8f8dc8ea778bc5b6aa610a00a4ea2
Mark Howells-Mead <mark@sayhello.ch>
2020-02-10 18:26:49 +0100

    Create block.js.jsx

---
+++ wordpress/Plugins/withAssets/assets/src/gutenberg/ist/block.js.jsx
@@ -0,0 +1,19 @@
+import { __ } from '@wordpress/i18n';
+import { registerBlockType } from '@wordpress/blocks';
+
+registerBlockType( 'sht/test', {
+	title: __( 'Test Block', 'sht' ),
+	description: __( 'Test Block description', 'sht' ),
+	icon: 'lock',
+	category: 'sht/blocks',
+	edit() {
+		return (
+			<p className={ "b-test-block" }>{ __('Translated String', 'sht') }</p>
+		);
+	},
+	save() {
+		return (
+			<p>Not translated String (because in save function)</p>
+		);
+	},
+} );
