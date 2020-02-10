5e200f596eae8491b2f087604f5bbfc610540275
Mark Howells-Mead <mark@sayhello.ch>
2020-02-10 18:26:10 +0100

    Create task-gutenberg.js

---
+++ wordpress/Plugins/withAssets/assets/gulp/task-gutenberg.js
@@ -0,0 +1,53 @@
+const filter = require('gulp-filter');
+
+import gulp from 'gulp';
+import webpack from 'webpack';
+import gulpWebpack from 'webpack-stream';
+import livereload from 'gulp-livereload';
+import rename from 'gulp-rename';
+import uglify from 'gulp-uglify';
+
+const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
+
+export const task = config => {
+	return gulp.src([
+			`${config.assetsBuild}gutenberg/blocks.js`
+		])
+		// Webpack
+		.pipe(
+			gulpWebpack({
+				mode: 'production',
+				module: {
+					rules: [{
+						test: /\.(js|jsx)$/,
+						exclude: /(node_modules)/,
+						loader: 'babel-loader'
+					}]
+				},
+				watchOptions: {
+					poll: true,
+					ignored: /node_modules/
+				},
+				output: {
+					filename: 'blocks.js',
+				},
+				plugins: [
+					new DependencyExtractionWebpackPlugin(),
+				],
+			}, webpack)
+		)
+		.on('error', config.errorLog)
+		.pipe(gulp.dest(config.distDir + 'gutenberg/'))
+
+		// Minify
+		.pipe(filter(['**/*.js']))
+		.pipe(uglify())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.on('error', config.errorLog)
+		.pipe(gulp.dest(config.distDir + 'gutenberg/'))
+
+		//reload
+		.pipe(livereload());
+};
