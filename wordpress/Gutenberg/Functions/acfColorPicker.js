f8462e8664e10539f32125e52a883f47c10db74c
Mark Howells-Mead <mark@sayhello.ch>
2020-04-16 15:43:36 +0200

    Update acfColorPicker.js

--- wordpress/Gutenberg/Functions/acfColorPicker.js
+++ wordpress/Gutenberg/Functions/acfColorPicker.js
@@ -1,9 +1,10 @@
 import domReady from '@wordpress/dom-ready';
+import { select } from '@wordpress/data';
 
 domReady(() => {
 	acf.add_filter('color_picker_args', function( args, field ){
 		let palette = [];
-		wp.data.select( "core/editor" ).getEditorSettings().colors.forEach(color => {
+		select('core/editor').getEditorSettings().colors.forEach(color => {
 			palette.push(color.color);
 		});
 		if(palette.length){
