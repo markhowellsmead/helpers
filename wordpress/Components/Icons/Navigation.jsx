0e3fb553adde4bf2dcac56b03ab36ca2965f1ba6
Mark Howells-Mead <mark@sayhello.ch>
2020-04-23 12:30:56 +0200

    Create Navigation.jsx

---
+++ wordpress/Components/Icons/Navigation.jsx
@@ -0,0 +1,20 @@
+/**
+ * Usage: import NavigationIcon from '../icons/navigation.jsx';
+ * mark@sayhello.ch 23.4.2020
+ * This example icon is from WordPress Gutenberg
+ */
+
+const Icon = () => (
+	<svg
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		xmlns="http://www.w3.org/2000/svg"
+		role="img"
+		aria-hidden="true"
+		focusable="false">
+		<path d="M12 4c-4.4 0-8 3.6-8 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm0 14.5c-3.6 0-6.5-2.9-6.5-6.5S8.4 5.5 12 5.5s6.5 2.9 6.5 6.5-2.9 6.5-6.5 6.5zM9 16l4.5-3L15 8.4l-4.5 3L9 16z"></path>
+	</svg>
+);
+
+export default Icon;
