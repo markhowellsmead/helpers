b105debc973c0042dc7b703426fcf8c25cba1a59
Mark Howells-Mead <mark@sayhello.ch>
2020-05-11 13:43:13 +0200

    Create PrimaryNaveWalker.php

---
+++ wordpress/Components/DropDownMenu/Vendor/PrimaryNaveWalker.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SayHello\Theme\Vendor;
+
+use Walker_Nav_Menu;
+
+class PrimaryNavWalker extends Walker_Nav_Menu
+{
+	public function start_lvl(&$output, $depth = 0, $args = null)
+	{
+		$indent = str_repeat(chr(9), $depth);
+		$output .= PHP_EOL.$indent.'<ul class="c-dropdownmenu__entries c-dropdownmenu__entries--level' .($depth+2). '">'.PHP_EOL;
+	}
+}
