08766f1f4c1e86d1539dd630b857991d547622ab
Mark Howells-Mead <mark@sayhello.ch>
2020-08-20 14:17:21 +0200

    Create backtotop.js

---
+++ wordpress/Components/BackToTop/backtotop.js
@@ -0,0 +1,4 @@
+document.documentElement.style.setProperty('--js-scrolloffset', window.innerHeight / 2);
+window.addEventListener('resize', function () {
+	document.documentElement.style.setProperty('--js-scrolloffset', window.innerHeight / 2);
+});
