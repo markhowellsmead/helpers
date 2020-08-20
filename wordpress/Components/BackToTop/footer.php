e325fabc63596db4ee83e47bd442280ecebb33ef
Mark Howells-Mead <mark@sayhello.ch>
2020-08-20 14:20:29 +0200

    Create footer.php

---
+++ wordpress/Components/BackToTop/footer.php
@@ -0,0 +1,6 @@
+<!-- Insert the following into the footer.php file -->
+
+<a href="#pagetop" class="o-backtotop">
+	<span class="o-backtotop__svg" aria-hidden="true"><?php include(get_template_directory().'/partials/svg/backtotop.svg');?></span>
+	<span class="screen-reader-text"><?php _ex('Zurück zum Seitenanfang', 'Button text', 'sht');?></span>
+</a>
