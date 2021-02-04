8cee2e8470d68745d3201e4f5e96b5fe4387bf89
Mark Howells-Mead <mark@sayhello.ch>
2021-02-04 18:41:42 +0100

    Update share.js

--- wordpress/Components/Share/share.js
+++ wordpress/Components/Share/share.js
@@ -1,25 +1,32 @@
 (function ($) {
+    var windowAtts = function (width, height) {
+        if (window.innerWidth > width && window.innerHeight > height) {
+            return (
+                'width=' +
+                width +
+                ',height=' +
+                height +
+                ',top=' +
+                (window.innerHeight / 2 - height / 2) +
+                ',left=' +
+                (window.innerWidth / 2 - width / 2)
+            );
+        } else {
+            return '';
+        }
+    };
 
-	var windowAtts = function (width, height) {
-		if(window.innerWidth > width && window.innerHeight > height) {
-			return 'width=' + width + ',height=' + height + ',top=' + (window.innerHeight / 2 - (height / 2)) + ',left=' + (window.innerWidth / 2 - (width / 2));
-		} else {
-			return '';
-		}
-	};
+    $('[data-shareto]').on('click.share', function (event) {
+        switch ($(this).data('shareto')) {
+            case 'facebook':
+                event.preventDefault();
+                window.open($(this).attr('href'), '_blank', windowAtts(710, 565));
+                break;
 
-	$('[data-shareto]').click(function (e) {
-		switch ($(this).data('shareto')) {
-		case 'facebook':
-			e.preventDefault();
-			window.open($(this).attr('href'), '_blank', windowAtts(710, 565));
-			break;
-
-		case 'twitter':
-			e.preventDefault();
-			window.open($(this).attr('href'), '_blank', windowAtts(550, 285));
-			break;
-		}
-	});
-
+            case 'twitter':
+                event.preventDefault();
+                window.open($(this).attr('href'), '_blank', windowAtts(550, 285));
+                break;
+        }
+    });
 })(jQuery);
