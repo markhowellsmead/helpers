2982f137813701d1c869dd3eb7ba044c9814bab0
Mark Howells-Mead <mark@sayhello.ch>
2021-09-01 11:43:03 +0200

    Update mmenu.js

--- wordpress/JavaScript/mmenu.js/mmenu.js
+++ wordpress/JavaScript/mmenu.js/mmenu.js
@@ -1,33 +1,8 @@
 // Mobile menu plugin
-// mmenujs.com/docs/custom-build.html
+// mmenujs.com
 
-//  Core
-import Mmenu from 'mmenu-js/dist/core/oncanvas/mmenu.oncanvas';
-
-//  Core add-ons
-import offcanvas from 'mmenu-js/dist/core/offcanvas/mmenu.offcanvas';
-import screenReader from 'mmenu-js/dist/core/screenreader/mmenu.screenreader';
-import scrollBugFix from 'mmenu-js/dist/core/scrollbugfix/mmenu.scrollbugfix';
-
-Mmenu.addons = {
-    offcanvas,
-    screenReader,
-    scrollBugFix,
-};
+import Mmenu from 'mmenu-js/dist/core/offcanvas/mmenu.offcanvas.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    new Mmenu(
-        '#mobile-menu',
-        {},
-        {
-            classNames: {
-                selected: 'c-menu__entry--current',
-            },
-            offCanvas: {
-                page: {
-                    selector: '.c-pagewrapper',
-                },
-            },
-        }
-    );
+    new Mmenu('#mobile-menu');
 });
