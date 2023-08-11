6ddb1aad1f8c73d757b9b638d9098ca37b14a492
Mark Howells-Mead <permanent.tourist@gmail.com>
2023-08-11 21:34:24 +0200

    Relocate CookieBanner Package

--- wordpress/Cookie Banner/cookiebanner.php
+++
@@ -1,4 +0,0 @@
-<div class="c-cookiebanner" aria-hidden="true">
-	<div class="c-cookiebanner__text"><?php echo get_field('sht_cookiebanner_text', 'options');?></div>
-	<button data-cookiebanner-close class="c-cookiebanner__button"><?php echo get_field('sht_cookiebanner_buttontext', 'options');?></button>
-</div>
