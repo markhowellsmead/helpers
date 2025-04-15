079496dd24b393ea77023d42e0ec588f7b1d0518
Mark Howells-Mead <mark@sayhello.ch>
2025-04-15 17:36:29 +0200

    Updated ImageSelector component

--- wordpress/Components/ImageSelector/index.jsx
+++ wordpress/Components/ImageSelector/index.jsx
@@ -1,62 +1,120 @@
-import { Button } from '@wordpress/components';
-import { Component, Fragment } from '@wordpress/element';
+/**
+ * Image selector component for WordPress Block Editor
+ * Image attribute is an object containing the ID.
+ * Last updated 15 th April 2025
+ * mark@sayhello.ch
+ *
+ * Usage:
+ *
+ 	image: {
+		type: 'object',
+		default: {
+			id: 0,
+		},
+	},
+	…
+	import ImageSelector from '../_components/ImageSelector/index.js';
+	…
+	const { image } = attributes;
+	<ImageSelector attributes={attributes} setAttributes={setAttributes} />
+ * 
+ **/
+
+import { BaseControl, Button } from '@wordpress/components';
 import { MediaUploadCheck, MediaUpload } from '@wordpress/block-editor';
 import { _x } from '@wordpress/i18n';
+import { useSelect } from '@wordpress/data';
 
-export default class extends Component {
-	constructor(props) {
-		super(...arguments);
-		this.props = props;
-	}
+import './index.scss';
 
-	render() {
-		const { allowedTypes, attributes, attributeKey, image, label, setAttributes } = this.props;
+const classNameBase = 'c-imageselector';
 
-		const allowed_types = allowedTypes || ['image/jpg', 'image/jpeg'];
-		const attribute_key = attributeKey || 'image';
+const PreviewImage = ({ image }) => {
+    if (!image?.media_details?.sizes?.medium?.source_url) {
+        return null;
+    }
 
-		return (
-			<div class="c-fileselector">
-				<MediaUploadCheck>
-					<MediaUpload
-						onSelect={image => {
-							setAttributes({ [attribute_key]: image });
-						}}
-						allowedTypes={allowed_types}
-						value={[attribute_key].id}
-						render={({ open }) => {
-							return (
-								<Fragment>
-									<label class="components-base-control__label" for="inspector-select-control-1">
-										{label}
-									</label>
-									<div className={`c-fileselector__figure ${!image.id ? 'c-fileselector__figure--nofile' : ''}`}>
-										{image.id && image.sizes && <img src={image.sizes.thumbnail.url} />}
-										{image.id && !image.sizes && <img src={image.url} />}
-										<div class="c-fileselector__buttons">
-											{!image.id && (
-												<Button onClick={open} isDefault isLarge isPrimary>
-													{_x('Datei auswählen', 'Admin component button text', 'sha')}
-												</Button>
-											)}
-											{image.id && (
-												<Fragment>
-													<Button onClick={open} isDefault isLarge>
-														{_x('Datei ersetzen', 'Admin component button text', 'sha')}
-													</Button>
-													<Button onClick={() => setAttributes({ image: { id: false } })} isDefault isSmall>
-														{_x('Datei entfernen', 'Admin component button text', 'sha')}
-													</Button>
-												</Fragment>
-											)}
-										</div>
-									</div>
-								</Fragment>
-							);
-						}}
-					/>
-				</MediaUploadCheck>
-			</div>
-		);
-	}
-}
+    const { source_url } = image.media_details.sizes.medium;
+    const { alt_text } = image;
+
+    return <img src={source_url} alt={alt_text} className={`${classNameBase}__image`} />;
+};
+
+const ImageSelector = ({
+    allowedTypes = ['image'],
+    attributes,
+    attributeKey = 'image',
+    setAttributes,
+    label,
+    withPreviewImage = false,
+}) => {
+    const imageAttr = attributes[attributeKey];
+    const imageId = imageAttr?.id;
+
+    const previewImage = useSelect(
+        select => (imageId != null ? select('core').getMedia(imageId) : null),
+        [imageId]
+    );
+
+    return (
+        <BaseControl label={label}>
+            <div className={classNameBase}>
+                <MediaUploadCheck>
+                    <MediaUpload
+                        onSelect={media => {
+                            setAttributes({ [attributeKey]: { id: media?.id } });
+                        }}
+                        allowedTypes={allowedTypes}
+                        value={imageId}
+                        render={({ open }) => (
+                            <figure
+                                className={`${classNameBase}__figure ${
+                                    !imageId ? `${classNameBase}__figure--noimage` : ''
+                                }`}
+                            >
+                                {withPreviewImage && previewImage && (
+                                    <PreviewImage image={previewImage} />
+                                )}
+                                <div className={`${classNameBase}__buttons`}>
+                                    {!imageId && (
+                                        <Button onClick={open} variant='primary'>
+                                            {_x(
+                                                'Bild auswählen',
+                                                'Admin component button text',
+                                                'sha'
+                                            )}
+                                        </Button>
+                                    )}
+                                    {!!imageId && (
+                                        <>
+                                            <Button onClick={open} variant='primary'>
+                                                {_x(
+                                                    'Bild ersetzen',
+                                                    'Admin component button text',
+                                                    'sha'
+                                                )}
+                                            </Button>
+                                            <Button
+                                                onClick={() => setAttributes({ [attributeKey]: 0 })}
+                                                variant='link'
+                                                isDestructive
+                                            >
+                                                {_x(
+                                                    'Bild entfernen',
+                                                    'Admin component button text',
+                                                    'sha'
+                                                )}
+                                            </Button>
+                                        </>
+                                    )}
+                                </div>
+                            </figure>
+                        )}
+                    />
+                </MediaUploadCheck>
+            </div>
+        </BaseControl>
+    );
+};
+
+export default ImageSelector;
