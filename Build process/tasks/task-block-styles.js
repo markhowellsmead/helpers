abe9da9fc6b39693d02cce43eaf3d502b6ea955e
Mark Howells-Mead <mark@sayhello.ch>
2025-10-29 17:57:43 +0100

    Add Gulp task for compiling block styles
    This Gulp task compiles SCSS files into CSS, applies autoprefixing, and outputs both regular and minified versions to the specified distribution directory.

---
+++ Build process/tasks/task-block-styles.js
@@ -0,0 +1,45 @@
+/**
+ * Gulp task to compile block styles.
+ *
+ * This file defines a Gulp task that processes SCSS files located in the specified block styles
+ * source directory. It compiles the SCSS files into CSS, applies autoprefixing, and outputs
+ * both regular and minified versions of the CSS files to the designated block styles distribution
+ * directory.
+ *
+ * It uses gulp-sass for compiling SCSS to CSS, gulp-autoprefixer for adding vendor prefixes,
+ * and cssnano for minifying the CSS files.
+ *
+ * This version 29.10.2025 mark@sayhello.ch
+ */
+
+import { src, dest } from 'gulp';
+import autoprefixer from 'gulp-autoprefixer';
+import cssnano from 'cssnano';
+import postcss from 'gulp-postcss';
+import rename from 'gulp-rename';
+import sass from 'gulp-sass';
+import * as dartSass from 'sass';
+
+const gulpSass = sass(dartSass);
+
+export const task = (config) => {
+	return (
+		src([config.blockStylesSrc])
+			.pipe(gulpSass().on('error', gulpSass.logError))
+			.pipe(autoprefixer())
+			.pipe(
+				rename((path) => ({
+					dirname: config.blockStylesDist.replace('./', '') + path.dirname.replace('/assets/src', '/assets/dist'),
+					basename: path.basename,
+					extname: path.extname,
+				}))
+			)
+			.pipe(dest('./'))
+			.on('error', config.errorLog)
+			// Minify
+			.pipe(postcss([cssnano()]))
+			.on('error', config.errorLog)
+			.pipe(rename({ suffix: '.min' }))
+			.pipe(dest('./'))
+	);
+};
